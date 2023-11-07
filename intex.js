4129566334ed5d45e09e4443047310a97cd3d8f8
unknown <muhammedsoubabkunnummek123@gmail.com>
2023-11-07 19:36:30 +0530

    first commit

---
+++ intex.js
@@ -0,0 +1,11 @@
+var largestOddNumber = function(num) {
+    for (let i = num.length - 1; i >= 0; i--) {
+        if (parseInt(num[i]) % 2 === 1) {
+            return num.slice(0, i + 1);
+        }
+    }
+    
+    return '';
+};
+
+console.log(largestOddNumber("53"))
